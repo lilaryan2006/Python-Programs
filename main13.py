713067e59186e50e669cbb78cac77d58b91ed985
Aryan Kumar <167889468+lilaryan2006@users.noreply.github.com>
2025-01-29 18:56:12 +0530

    adding new file

---
+++ main13.py
@@ -0,0 +1,15 @@
+n=int(input('Enter the number of day'))
+if (n==1):
+         print('Monday')
+elif (n==2):
+         print('Tuesday')
+elif (n==3):
+         print('wednesady')
+elif (n==4):
+         print('Thursday')
+elif (n==5):
+         print('Friday')
+elif (n==6):
+         print('saturday')
+else:
+         print('The day is Sunday')     
